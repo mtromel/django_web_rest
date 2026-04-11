0854247c92169f31862e32c6c89c47ff823159ad
Marcos Tromel <marcos.tromel@gmail.com>
2026-04-11 12:14:37 -0300

    add media handling, recipe models, and category view template

--- projeto/settings.py
+++ projeto/settings.py
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)  # type: ignore
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)  # type: ignore
+    description = models.CharField(max_length=165)  # type: ignore
+    slug = models.SlugField()  # type: ignore
+    preparation_time = models.IntegerField()  # type: ignore
+    preparation_time_unit = models.CharField(max_length=65)  # type: ignore
+    servings = models.IntegerField()  # type: ignore
+    servings_unit = models.CharField(max_length=65)  # type: ignore
+    preparation_steps = models.TextField()  # type: ignore
+    preparation_steps_is_html = models.BooleanField(
+        default=False)  # type: ignore
+    created_at = models.DateTimeField(auto_now_add=True)  # type: ignore
+    updated_at = models.DateTimeField(auto_now=True)  # type: ignore
+    is_published = models.BooleanField(default=False)  # type: ignore
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/')  # type: ignore
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)  # type: ignore
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)  # type: ignore
+
+    def __str__(self):
+        return self.title
