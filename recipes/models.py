fc6cfdba570a141dbf46226517c8ec175a1c4624
Marcos Tromel <marcos.tromel@gmail.com>
2026-04-12 14:51:14 -0300

    add category field to Recipe model and update recipe template for conditional rendering

--- recipes/models.py
+++ recipes/models.py
@@ -26,7 +26,8 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/')  # type: ignore
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)  # type: ignore
+        Category, on_delete=models.SET_NULL, null=True,
+        blank=True, default=None)  # type: ignore
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)  # type: ignore
 
