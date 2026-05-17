f2979826b7e60d0efb8956ff8e720ad83fd29c01
Marcos Tromel <marcos.tromel@gmail.com>
2026-05-17 18:14:45 -0300

    fix: update recipe view title to display recipe title dynamically and optimize recipe retrieval

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 
@@ -30,10 +30,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(
         request,
