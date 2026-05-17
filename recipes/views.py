ad44081c548d26968a3b4f588bf3f842200b7903
Marcos Tromel <marcos.tromel@gmail.com>
2026-05-17 17:58:50 -0300

    fix: update recipe view logic to handle preparation steps and improve category retrieval

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404
 from recipes.models import Recipe
 
 
@@ -15,26 +13,32 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not found 😒')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
 
     return render(
         request,
         'recipes/pages/category.html',
         context={
             'recipes': recipes,
-            'title': f'{recipes.first().category.name} - Category | ',
+            'title': f'{recipes[0].category.name} - Category | ',
         })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(
         request,
         'recipes/pages/recipe-view.html',
         context={
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail_page': True,
         })
