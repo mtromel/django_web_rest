b484332574941fb03da4cfffea78c828af8258e7
Marcos Tromel <marcos.tromel@gmail.com>
2026-05-19 22:07:19 -0300

    feat: add pytest configuration and initial test example

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,11 @@
+"""Tests for recipe URL routes."""
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    """Tests for recipe URL routes."""
+
+    def test_the_pytest_is_ok(self):
+        """Test that pytest is working."""
+        assert 1 == 1, 'Um é igual a um'
