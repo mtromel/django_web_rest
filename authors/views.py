e8fb8cbc9102ef9bc08dca60d263e45e3cb8b1bf
mtromel <marcos.tromel@gmail.com>
2026-06-20 11:40:03 -0300

    feat: implement registration form view and template

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,11 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
     return render(
         request,
         "authors/pages/register_view.html",
