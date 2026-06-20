02e491fe6a047af07f734235fbf82d833af27d80
mtromel <marcos.tromel@gmail.com>
2026-06-20 11:16:52 -0300

    feat: add authors app with registration functionality

---
+++ authors/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .forms import RegisterForm
+
+
+def register_view(request):
+    form = RegisterForm()
+    return render(
+        request,
+        "authors/pages/register_view.html",
+        {
+            "form": form,
+        },
+    )
