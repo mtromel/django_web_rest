a262112913204b73e75da6b3507510b3bfc1b108
mtromel <marcos.tromel@gmail.com>
2026-08-16 09:16:52 -0300

    feat: refactor templates to use global partials and add new styles

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterForm
 
@@ -14,6 +15,7 @@ def register_view(request):
         "authors/pages/register_view.html",
         {
             "form": form,
+            "form_action": reverse("authors:create"),
         },
     )
 
