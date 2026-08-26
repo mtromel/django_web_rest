6672d35a65eaf3a492ff19626c49683eb30bdfb6
mtromel <marcos.tromel@gmail.com>
2026-08-26 12:46:08 -0300

    feat: implement dashboard view and login message partial for authenticated users

--- authors/views.py
+++ authors/views.py
@@ -59,7 +59,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse("authors:login")
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -74,7 +73,7 @@ def login_create(request):
     else:
         messages.error(request, "Invalid username or password")
 
-    return redirect(login_url)
+    return redirect(reverse("authors:dashboard"))
 
 
 @login_required(login_url="authors:login", redirect_field_name="next")
@@ -91,3 +90,8 @@ def logout_view(request):
     logout(request)
 
     return redirect(reverse("authors:login"))
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard(request):
+    return render(request, "authors/pages/dashboard.html")
