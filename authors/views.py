83f230b59fe7eb55f2ce50d3363b236b07735b1f
mtromel <marcos.tromel@gmail.com>
2026-08-29 08:57:45 -0300

    feat: add recipe management features to dashboard and implement recipe editing page

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from recipes.models import Recipe
+
 from .forms import LoginForm, RegisterForm
 
 
@@ -94,4 +96,35 @@ def logout_view(request):
 
 @login_required(login_url="authors:login", redirect_field_name="next")
 def dashboard(request):
-    return render(request, "authors/pages/dashboard.html")
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+    )
+    return render(
+        request,
+        "authors/pages/dashboard.html",
+        context={
+            "recipes": recipes,
+        },
+    )
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard_recipe_edit(
+    request,
+    id,
+):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    )
+
+    if not recipe:
+        raise Http404()
+
+    return render(
+        request,
+        "authors/pages/dashboard_recipe.html",
+        context={},
+    )
