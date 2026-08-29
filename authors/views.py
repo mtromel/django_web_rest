c8d758157349ca2cb45b64939d9087eb3ecc8296
mtromel <marcos.tromel@gmail.com>
2026-08-29 11:51:28 -0300

    feat: implement AuthorRecipeForm and enhance dashboard recipe editing functionality

--- authors/views.py
+++ authors/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 from recipes.models import Recipe
 
-from .forms import LoginForm, RegisterForm
+from .forms import AuthorRecipeForm, LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -118,13 +118,29 @@ def dashboard_recipe_edit(
         is_published=False,
         author=request.user,
         pk=id,
-    )
+    ).first()
 
     if not recipe:
         raise Http404()
 
+    form = AuthorRecipeForm(
+        data=request.POST or None, files=request.FILES or None, instance=recipe
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Sua receita foi salva com sucesso!")
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
+
     return render(
         request,
         "authors/pages/dashboard_recipe.html",
-        context={},
+        context={"form": form},
     )
