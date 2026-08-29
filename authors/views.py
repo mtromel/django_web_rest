54012a6bc58bdfa1ab6f3d72ccf8b443cf5eaab3
mtromel <marcos.tromel@gmail.com>
2026-08-29 12:44:59 -0300

    feat: add new recipe creation functionality and update login message

--- authors/views.py
+++ authors/views.py
@@ -144,3 +144,26 @@ def dashboard_recipe_edit(
         "authors/pages/dashboard_recipe.html",
         context={"form": form},
     )
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(data=request.POST or None, files=request.FILES or None)
+
+    if form.is_valid():
+        recipe: Recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Sua receita foi salva com sucesso!")
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(recipe.id,)))
+
+    return render(
+        request,
+        "authors/pages/dashboard_recipe.html",
+        context={"form": form, "form-action": reverse("authors:dashboard_recipe_new")},
+    )
