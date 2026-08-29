f1731e3e6cb666938dcdef01d28a9915368ca8b4
mtromel <marcos.tromel@gmail.com>
2026-08-29 13:21:10 -0300

    feat: implement validation methods in AuthorRecipeForm and add is_positive_number utility function

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,12 +1,18 @@
+from collections import defaultdict
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from recipes.models import Recipe
 from utils.django_forms import add_attr
+from utils.strings import is_positive_number
 
 
 class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._myerrors = defaultdict(list)
 
         add_attr(self.fields.get("preparation_steps"), "class", "span-2")
         add_attr(self.fields.get("cover"), "class", "span-2")
@@ -39,3 +45,44 @@ class AuthorRecipeForm(forms.ModelForm):
                 )
             ),
         }
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cd = self.cleaned_data
+        title = cd.get("title")
+        description = cd.get("description")
+
+        if title == description:
+            self._myerrors["title"].append("Cannot be equal to description")
+            self._myerrors["description"].append("Cannot be equal to title")
+
+        if self._myerrors:
+            raise ValidationError(self._myerrors)
+
+        return super_clean
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+
+        if len(title) < 5:
+            self._myerrors["title"].append("Must have at least 5 chars.")
+
+        return title
+
+    def clean_preparation_time(self):
+        field_name = "preparation_time"
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._myerrors[field_name].append("Must be a positive number.")
+
+        return field_value
+
+    def clean_servings(self):
+        field_name = "servings"
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._myerrors[field_name].append("Must be a positive number.")
+
+        return field_value
