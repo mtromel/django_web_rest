9d35a0729a263661f06b745daa5ddf15c25cb0be
mtromel <marcos.tromel@gmail.com>
2026-08-30 10:04:45 -0300

    feat: refactor recipe management views to use class-based views and streamline URL routing

--- authors/views/all.py
+++ authors/views/all.py
@@ -109,43 +109,6 @@ def dashboard(request):
 
 
 @login_required(login_url="authors:login", redirect_field_name="next")
-def dashboard_recipe_edit(
-    request,
-    id,
-):
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    form = AuthorRecipeForm(
-        data=request.POST or None, files=request.FILES or None, instance=recipe
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, "Sua receita foi salva com sucesso!")
-        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
-
-    return render(
-        request,
-        "authors/pages/dashboard_recipe.html",
-        context={"form": form},
-    )
-
-
-@login_required(login_url="authors:login", redirect_field_name="next")
 def dashboard_recipe_new(request):
     form = AuthorRecipeForm(data=request.POST or None, files=request.FILES or None)
 
@@ -166,25 +129,3 @@ def dashboard_recipe_new(request):
         "authors/pages/dashboard_recipe.html",
         context={"form": form, "form-action": reverse("authors:dashboard_recipe_new")},
     )
-
-
-@login_required(login_url="authors:login", redirect_field_name="next")
-def dashboard_recipe_delete(request):
-    if not request.POST:
-        raise Http404()
-
-    POST = request.POST
-    id = POST.get("id")
-
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    recipe.delete()
-    messages.success(request, "Delete successfully")
-    return redirect(reverse("authors:dashboard"))
