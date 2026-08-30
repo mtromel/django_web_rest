94af76ae973b33e8ec40d1aa842f89e60ed2f978
mtromel <marcos.tromel@gmail.com>
2026-08-30 10:38:48 -0300

    feat: implement RecipeListViewBase for home page and update URL routing

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,38 @@ import os
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
 
 PER_PAGE = int(os.environ.get("PER_PAGE", 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = "recipes"
+    paginate_by = None
+    ordering = "-id"
+    template_name = "recipes/pages/home.html"
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            is_published=True,
+        )
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        page_object, pagination_range = make_pagination(
+            self.request, ctx.get("recipes"), PER_PAGE
+        )
+        ctx.update({"recipes": page_object, "pagination_range": pagination_range})
+        return ctx
 
 
 def home(request):
