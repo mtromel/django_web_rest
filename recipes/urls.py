f376e15af7314caa30aa1892fcfae8cdb5849d5d
mtromel <marcos.tromel@gmail.com>
2026-09-06 10:30:41 -0300

    feat: add API views for recipe list and detail with JSON responses

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
         name="category",
     ),
     path("recipes/<int:pk>/", views.RecipeDetail.as_view(), name="recipe"),
+    path(
+        "recipes/api/v1/", views.RecipeListViewHomeApi.as_view(), name="recipes_api_v1"
+    ),
+    path(
+        "recipes/api/v1/<int:pk>/",
+        views.RecipeDetailApi.as_view(),
+        name="recipes_api_v1_detail",
+    ),
 ]
