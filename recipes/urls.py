ccd05589a4b12b3d2b4e210295890d6e4521bf79
mtromel <marcos.tromel@gmail.com>
2026-09-07 09:52:51 -0300

    Implement API Json

--- recipes/urls.py
+++ recipes/urls.py
@@ -22,9 +22,3 @@ urlpatterns = [
         name="recipes_api_v1_detail",
     ),
 ]
-#     path(
-#         "recipes/api/v1/<int:pk>/",
-#         views.RecipeDetailApi.as_view(),
-#         name="recipes_api_v1_detail",
-#     ),
-# ]
