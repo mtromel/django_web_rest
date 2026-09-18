6c64650221c56812fb63c6cd896605ffbc1c36cc
mtromel <marcos.tromel@gmail.com>
2026-09-18 20:49:57 -0300

    Refactor code structure for improved readability and maintainability

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,5 +1,6 @@
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
+from .environment import BASE_DIR
 
 LANGUAGE_CODE = "pt-br"
 
@@ -8,3 +9,7 @@ TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,8 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
 from django.shortcuts import render
+from django.utils import translation
+from django.utils.translation import gettext as _
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
@@ -53,7 +55,16 @@ class RecipeListViewBase(ListView):
         page_object, pagination_range = make_pagination(
             self.request, ctx.get("recipes"), PER_PAGE
         )
-        ctx.update({"recipes": page_object, "pagination_range": pagination_range})
+
+        html_language = translation.get_language()
+
+        ctx.update(
+            {
+                "recipes": page_object,
+                "pagination_range": pagination_range,
+                "html_language": html_language,
+            }
+        )
         return ctx
 
 
@@ -89,9 +100,11 @@ class RecipeListViewCategory(RecipeListViewBase):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
 
+        category_translation = _("Category")
+
         ctx.update(
             {
-                "title": f"{ctx.get('recipes')[0].category.name} - Category | ",
+                "title": f"{ctx.get('recipes')[0].category.name} - {category_translation} | ",
             }
         )
 
