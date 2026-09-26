077936ca5a519c2891d3d63c0297de351b277897
mtromel <marcos.tromel@gmail.com>
2026-09-26 11:20:41 -0300

    feat: add create method to RecipeAPIv2ViewSet for handling recipe creation

--- project/settings/rest_framework.py
+++ project/settings/rest_framework.py
@@ -7,6 +7,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -49,6 +50,15 @@ class RecipeAPIv2ViewSet(ModelViewSet):
             ]
 
         return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
