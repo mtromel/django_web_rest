4ac460e284fb9189683bc3c6ab228d473112867b
mtromel <marcos.tromel@gmail.com>
2026-09-26 09:50:36 -0300

    feat: add IsOwner permission and update RecipeAPIv2ViewSet for object-level permissions

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
 from ..models import Recipe
+from ..permissions import IsOwner
 from ..serializers import RecipeSerializer, TagSerializer
 
 
@@ -18,6 +20,9 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [  # noqa: RUF012
+        IsAuthenticatedOrReadOnly,
+    ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -29,10 +34,24 @@ class RecipeAPIv2ViewSet(ModelViewSet):
 
         return qs
 
-    def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
+    def get_object(self):
+        pk = self.kwargs.get("pk", "")
+        obj = get_object_or_404(self.get_queryset(), pk=pk)
 
-        recipe = self.get_queryset().filter(pk=pk).first()
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [
+                IsOwner(),
+            ]
+
+        return super().get_permissions()
+
+    def partial_update(self, request, *args, **kwargs):
+        recipe = self.get_object()
         serializer = RecipeSerializer(
             instance=recipe,
             data=request.data,
